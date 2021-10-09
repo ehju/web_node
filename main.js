ce101fd26d3d93defcb9326628b98fc48a9b5454
action <junejc0613@gmail.com>
2021-10-09 02:50:14 +0000

    filelist불러오기  middleware로 분리

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ const sanitizeHtml = require('sanitize-html');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 var template = require('./lib/template.js');
-
+var getList = (req, res, next) => {
+  fs.readdir('./data', (error, flist) => {
+    req.list = flist;
+    next();
+  });
+};
 
 app.use(express.static('public'));
 
@@ -16,71 +21,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(compression());
 
+app.get('*',getList);
+
 app.get('/', (req, res) => {
-  fs.readdir('./data', (error, flist) => {
-    var title = 'Welcome';
-    var context = 'Hello, Node.js';
-    var list = template.list(flist);
-    var html = template.html(
-      title,
-      list,
-      `<h2>${title}</h2><p>${context}</p><img src="/images/coding.jpg" style="width:500px; margin:10px">`,
-      `<button><a href="/create">new post</a></button>`
-    );
-    res.send(html);
-  });
+  var title = 'Welcome';
+  var context = 'Hello, Node.js';
+  var list = template.list(req.list);
+  var html = template.html(
+    title,
+    list,
+    `<h2>${title}</h2><p>${context}</p><img src="/images/coding.jpg" style="width:500px; margin:10px">`,
+    `<button><a href="/create">new post</a></button>`
+  );
+  res.send(html);
 });
 
 app.get('/page/:pageId', (req, res) => {
-  fs.readdir('./data', (error, flist) => {
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, context) => {
-      var list = template.list(flist);
-      var title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedContext = sanitizeHtml(context, {
-        allowedTags: [
-          'h1',
-          'h2',
-          'h3',
-          'h4',
-          'h5',
-          'h6',
-          'p',
-          'strong',
-          'em',
-          'br',
-          'li',
-          'ol',
-          'ul',
-          'span',
-          'blockquote',
-        ],
-      });
-      var html = template.html(
-        title,
-        list,
-        `<h2>${sanitizedTitle}</h2> <p>${sanitizedContext}</p>`,
-        `<button><a href="/create">new post</a></button>
+  var filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, context) => {
+    var list = template.list(req.list);
+    var title = req.params.pageId;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedContext = sanitizeHtml(context, {
+      allowedTags: [
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'p',
+        'strong',
+        'em',
+        'br',
+        'li',
+        'ol',
+        'ul',
+        'span',
+        'blockquote',
+      ],
+    });
+    var html = template.html(
+      title,
+      list,
+      `<h2>${sanitizedTitle}</h2> <p>${sanitizedContext}</p>`,
+      `<button><a href="/create">new post</a></button>
                  <button><a href="/update/${title}">update</a></button>
                  <form action="/delete_process" method="post" style="display:inline;">
                      <input type="hidden" name="id" value="${title}">
                      <input type="submit" value="delete">
                  </form>`
-      );
-      res.send(html);
-    });
+    );
+    res.send(html);
   });
 });
 
 app.get('/create', (req, res) => {
-  fs.readdir('./data', (error, flist) => {
-    var title = 'WEB - create';
-    var list = template.list(flist);
-    var html = template.html(
-      title,
-      list,
-      `
+  var title = 'WEB - create';
+  var list = template.list(req.list);
+  var html = template.html(
+    title,
+    list,
+    `
     <form action="/create_process" method="post">
         <p><input type ="text" name="title"></p>
         <textarea id="editor" name="description" ></textarea>
@@ -93,10 +95,9 @@ app.get('/create', (req, res) => {
             console.error( error );
         } );
     </script>`,
-      `<button><a href="/create">new post</a></button>`
-    );
-    res.send(html);
-  });
+    `<button><a href="/create">new post</a></button>`
+  );
+  res.send(html);
 });
 
 // create process
@@ -116,15 +117,14 @@ app.post('/create_process', (req, res) => {
 
 //update
 app.get('/update/:pageId', (req, res) => {
-  fs.readdir('./data', (error, flist) => {
-    var title = req.params.pageId;
-    var list = template.list(flist);
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, context) => {
-      var html = template.html(
-        title,
-        list,
-        `
+  var title = req.params.pageId;
+  var list = template.list(req.list);
+  var filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, context) => {
+    var html = template.html(
+      title,
+      list,
+      `
             <form action="/update_process" method="post">
             <input type ="hidden" name="id" value=${title}>
             <p><input type ="text" name="title" value=${title}></p>
@@ -138,10 +138,9 @@ app.get('/update/:pageId', (req, res) => {
             console.error( error );
         } );
     </script>`,
-        `<button><a href="/create">new post</a></button>`
-      );
-      res.send(html);
-    });
+      `<button><a href="/create">new post</a></button>`
+    );
+    res.send(html);
   });
 });
 
