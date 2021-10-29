764661023bd3946b2208710b167bf076faf50a57
action <junejc0613@gmail.com>
2021-10-29 11:47:52 +0000

    practice: cookie practice

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,11 +1,26 @@
 var http = require('http');
+var cookie = require('cookie');
 http.createServer(function(request, response){
+  //console.log(request.headers.cookie);
+  var cookies = {};
+  if(request.headers.cookie !==undefined){
+    cookies= cookie.parse(request.headers.cookie);
+  }
+  console.log(cookies);
   //response.setHeader('Content-Type', 'text/html');
   //response.setHeader('X-Foo', 'bar');
   //response.writeHead(200, { 'Content-Type': 'text/plain' });
   //response.end('ok');
-  //response.writeHead(200,{
-  //  'Set-Cookie':['yummy_cookie=choco','tasty_cookie=strawberry']
-  //});
+  response.writeHead(200,{
+    'Set-Cookie':[
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      `Permanent=cookies; Max-Age=${60*60*24*30}`,
+      'Secure=Secure; Secure',
+      'HttpOnly=HttpOnly; HttpOnly',
+      'Path=Path; Path=/cookie',
+      'Domain=Domain; Domain=.goorm.io'
+    ]
+  });
   response.end('Cookie!!');
 }).listen(5000);
